72040f6fe26e5a095655d630c0ea9c5c9ef149bb
Reazul Hoque <engrreaz@gmail.com>
2026-01-25 23:25:34 +0600

    Upload

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ function student_profile_image_path($student_id)
     foreach ($possible_extensions as $ext) {
         $file_path = dirname(__DIR__) . '/students/' . $student_id . '.' . $ext;
         if (file_exists($file_path)) {
-            return $BASE_PATH_URL . 'students/' . $student_id . '.' . $ext;
+            return $BASE_PATH_URL_FILE . 'students/' . $student_id . '.' . $ext;
         }
     }
     return $BASE_PATH_URL . 'students/noimg.jpg';
