ef7706d99e72876a137fd37ec66e8c5e0a5ef15a
Reazul Hoque <engrreaz@gmail.com>
2026-02-02 18:49:07 +0600

    Upload

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $page_title = "Dashboard";
 $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
 $url = $protocol . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 echo $url;
-
+echo '<hr>' . $_GET['lbl'];
 $hour = date('H');
 $greeting = ($hour < 12) ? "Good Morning" : (($hour < 17) ? "Good Afternoon" : "Good Evening");
 ?>
