8ee310ce1a69b7b062a503f91fd399c07a8e9bd1
Reazul Hoque <engrreaz@gmail.com>
2026-02-05 23:26:42 +0600

    Upload

--- ajax/load_students.php
+++ ajax/load_students.php
@@ -1,6 +1,9 @@
 <?php
-header('Content-Type: application/json');
-error_reporting(0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 include_once '../inc.light.php';
 include_once '../datam/datam-stprofile.php';
 
