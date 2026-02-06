6c60e9da5bda09441cff477a8b61af290ac589ef
Reazul Hoque <engrreaz@gmail.com>
2026-02-06 22:36:16 +0600

    Upload

--- header.php
+++ header.php
@@ -172,7 +172,7 @@
             <div class="rounded-circle overflow-hidden border top-avatar shadow-sm <?= $ringClass ?>"
                 style="width:34px;height:34px;cursor:pointer;z-index:25999;" onclick="toggleAvatarMenu()">
 
-                <img src="<?= $pth ?>" style="width:100%;" >
+                <img src="<?= $pth ?>" >
 
             </div>
 
