c9912b73e1f9d317c623883b424f22b39dd4b160
Reazul Hoque <engrreaz@gmail.com>
2026-05-08 16:32:55 +0600

    Fix Errors

--- push.php
+++ push.php
@@ -1,7 +1,8 @@
 <?php
 require 'functions-fcm.php';
 define('API_ACCESS_KEY', 'AAAAiSanis8:APA91bGHIRxAjn8YBaf562fukaYy9N9_8LiNIm5XcTZnHEPqIK7Nr38PQhMJrWTpt9g0VI6U9DMvRT58K-D8AwHwwBvG3YqK8hKbxTMNu9qjaAm6KGj09FGyYT3RVUwExfs4IWXSfucp'); // Replace YOUR FIREBASE CLOUD MESSAGING API KEY with your Firebase Cloud Messaging server Key
-$token = 'd9bEq1MSSBOkO-DtXB5cEJ:APA91bEmc_dZnL3mkaHh37aPXLrwaKRTIMSSynseNMWChXf_WuZk8b5Ns5WT39tkJz5DlpYJK5kpohN8yvGEHK8GnPCfzWespwD8CWjfWMXk4DydCWAxc4Y';
+// $token = 'd9bEq1MSSBOkO-DtXB5cEJ:APA91bEmc_dZnL3mkaHh37aPXLrwaKRTIMSSynseNMWChXf_WuZk8b5Ns5WT39tkJz5DlpYJK5kpohN8yvGEHK8GnPCfzWespwD8CWjfWMXk4DydCWAxc4Y';
+$token = 'fJCTf1zdTc6WiaAbJ8PYB2:APA91bEgZYqfDo2-7N1ylHXHXA003Bws1tk3T47nF-UGuLuW2e0bj4i8nJV8cKevdyd_9KZ-XdQj78C42VfeK2iHO6_MNgcg_9xQhJ-72RvrzJjhHq46Whg';
 
 
 $ttm = date("H:i:s");
