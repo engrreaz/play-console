529102a12eb8a5cdbc006f33e7489927d264a618
Reazul Hoque <engrreaz@gmail.com>
2026-05-11 02:51:26 +0600

    Fix Errors

--- push.php
+++ push.php
@@ -6,7 +6,7 @@ include 'inc.light.php';
 //     'dRx0KEj0QwOCm0aJr7mb71:APA91bFHZ5lpsPANkvI_zU7Oo34W9z7rSw1UIIQCD_1kwFHkDXMsvDxiDnupLJUTIP2hWkHwiVL97clXs0cNgCYTzkgdgsVqLm5JARSX8tGsp5sgbi4_RcA',
 // ];
 
-// $tokens[] = $token;
+$tokens[] = $token;
 // pushFCM($tokens, 'Hello', 'Test Message, This is for check', 'https://playconsole.eimbox.com/notific-icon/fingerprint.png');
 // /*
 $ttm = date("H:i:s");
