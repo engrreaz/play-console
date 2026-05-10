b653aadc2136292090fa7a8277e9aad916d7488a
Reazul Hoque <engrreaz@gmail.com>
2026-05-10 19:32:10 +0600

    Fix Errors

--- get-widget-data.php
+++ get-widget-data.php
@@ -6,7 +6,7 @@ $txt1 = '';
 $txt2 = '';
 $txt3 = '';
 $progress = 0;
-$in_time = '';
+$in_time = '09:00:00';
 $out_time = '';
 
 $attnd = mysqli_fetch_row(mysqli_query($conn,"SELECT COUNT(yn) FROM stattnd WHERE adate='$td' AND sccode='$sccode' AND yn=1"))[0] ?? 0;
