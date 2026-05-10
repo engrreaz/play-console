0e4ee27eb0fc757be4f14b535289e4d575f58ce7
Reazul Hoque <engrreaz@gmail.com>
2026-05-10 23:24:19 +0600

    Fix Errors

--- push.php
+++ push.php
@@ -2,7 +2,9 @@
 
 require 'functions-fcm.php';
 
-$token = 'dRx0KEj0QwOCm0aJr7mb71:APA91bFHZ5lpsPANkvI_zU7Oo34W9z7rSw1UIIQCD_1kwFHkDXMsvDxiDnupLJUTIP2hWkHwiVL97clXs0cNgCYTzkgdgsVqLm5JARSX8tGsp5sgbi4_RcA';
+$tokens = [
+    'dRx0KEj0QwOCm0aJr7mb71:APA91bFHZ5lpsPANkvI_zU7Oo34W9z7rSw1UIIQCD_1kwFHkDXMsvDxiDnupLJUTIP2hWkHwiVL97clXs0cNgCYTzkgdgsVqLm5JARSX8tGsp5sgbi4_RcA',
+];
 
 $ttm = date("H:i:s");
 
@@ -10,56 +12,61 @@ $accessToken = getAccessToken();
 
 $projectId = 'eimbox-9014d';
 
-$url = "https://fcm.googleapis.com/v1/projects/".$projectId."/messages:send";
+$url = "https://fcm.googleapis.com/v1/projects/" . $projectId . "/messages:send";
 
-$data = [
-    "message" => [
+foreach ($tokens as $token) {
 
-        "token" => $token,
+    $data = [
+        "message" => [
+            "topic" => "Administrator",
 
-        "notification" => [
-            "title" => "Hello",
-            "body" => "Test Message @ ".$ttm
-        ],
+            "token" => $token,
 
-        "data" => [
-            "title" => "EIMBox আপডেট",
-            "body" => "আপনার প্রোফাইল চেক করুন ".$ttm,
-            "image" => "https://eimbox.com/images/fav.png",
-            "m_icon" => "baseline_fingerprint_24",
-            "data1" => $ttm
-        ],
-
-        "android" => [
             "notification" => [
+                "title" => "Hello",
+                "body" => "Test Message @ " . $ttm
+            ],
+
+            "data" => [
+                "title" => "EIMBox আপডেট",
+                "body" => "আপনার প্রোফাইল চেক করুন " . $ttm,
                 "image" => "https://eimbox.com/images/fav.png",
-                "sound" => "default",
-                "channel_id" => "default_channel"
+                "m_icon" => "baseline_fingerprint_24",
+                "data1" => $ttm
+            ],
+
+            "android" => [
+                "notification" => [
+                    "image" => "https://eimbox.com/images/fav.png",
+                    "sound" => "default",
+                    "channel_id" => "default_channel"
+                ]
             ]
+
         ]
+    ];
 
-    ]
-];
+    $headers = [
+        "Authorization: Bearer " . $accessToken,
+        "Content-Type: application/json"
+    ];
 
-$headers = [
-    "Authorization: Bearer ".$accessToken,
-    "Content-Type: application/json"
-];
+    $ch = curl_init();
 
-$ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    $response = curl_exec($ch);
 
-$response = curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo curl_error($ch);
+    } else {
+        echo $response;
+    }
 
-if(curl_errno($ch)){
-    echo curl_error($ch);
-}else{
-    echo $response;
+    curl_close($ch);
+
 }
-
-curl_close($ch);
