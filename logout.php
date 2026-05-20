3029b2c9c70e0408110589061d7dc6559d758a0b
Reazul Hoque <engrreaz@gmail.com>
2026-05-20 11:51:03 +0600

    Fix Errors

--- logout.php
+++ logout.php
@@ -1,10 +1,26 @@
 <?php
 session_start();
-session_unset();
+
+$_SESSION = array();
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+
 session_destroy();
 
-// কুকিও ডিলিট করে দিতে পারেন যেহেতু আপনি কুকি ব্যবহার করেছেন
-setcookie("user", "", time() - 3600, "/"); 
+// আপনার custom cookie delete
+setcookie("user", "", time() - 3600, "/");
 
 header("Location: login.php");
 exit();
