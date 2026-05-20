352f63f66895c05bfdf8d1d270d310f8a35435e3
Reazul Hoque <engrreaz@gmail.com>
2026-05-20 19:27:23 +0600

    Fix Errors

--- index_asstt_teacher.php
+++ index_asstt_teacher.php
@@ -335,8 +335,11 @@ $greet = ($hr < 12) ? "Good Morning" : (($hr < 17) ? "Good Afternoon" : "Good Ev
             <?php
             foreach ($blocks as $id => $info):
                 $valid_user = $info['role'] ?? '';
-                echo $valid_user;
+                echo $valid_user . '/' . $userlevel . '<br>';
                 $roles = explode('|', $valid_user);
+                foreach($roles as $r){
+                    echo '--' . $r . '--<br>';
+                }
                 if (in_array($userlevel, $roles)) {
                     ?>
                     <div class="block-unit shadow-sm" id="block-<?php echo $id; ?>" data-id="<?php echo $id; ?>">
